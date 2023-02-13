83d6575de9a0fef73ae714bab82d7d634d1c07c0
Janith-Nanayakkara <janithnanayakkara0202@gmail.com>
2023-02-13 12:47:39 +0530

    Content update

--- Snake_Game.py
+++ Snake_Game.py
@@ -11,6 +11,32 @@ def change(x,y):
     aim.y=y
 
     def inside(head):
-        return -200 < head.x
+        return -200 < head.x < 190 and -200 < head.y < 190
+    def move ():
+        head = snake.copy()
+        head.move(aim)
+
+        if not inside(head) or head in snake:
+            square(head.x,head.y,9,'red')
+            update()
+            return
+
+        snake.append()
+
+        if head == food:
+            print('snake',len(snake))
+            food.x = randrange(-15,15)*10
+            food.y = randrange(-15,15)*10
+
+        else:
+            snake.pop(0)
+            clear()
+
+            for body in snake:
+                square(body.x,body.y,9,'green')
+
+                square(food.x,food.y,9,'red')
+                update()
+                ontimer(move,100)
 
 
